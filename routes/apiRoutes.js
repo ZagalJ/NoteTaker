d9baeb9dffdb1396cd08813e6c8dd823f40fcfb6
ZagalJ <jonathan.zagal@hotmail.com>
2021-09-08 00:51:11 -0500

    cleaned code and remove unused code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,3 @@
-// const notesData = require ("../db/db.json");
-
-// module.exports = function(app){
-//     app.get("/api/notes", function(req,res){
-//         readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-//     });
-//     app.post("/api/notes", function(req,res){
-//         notesData.push(req.body);
-//         res.json(true);
-//     });
-
-// }
-
 const fs = require("fs")
 const path = require("path")
 
@@ -20,7 +7,6 @@ app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "../db/db.
  //API for storing user added note and renderning updated  notes stored on db.json
  app.post("/api/notes", (req, res) => {
      let newNote ={
-         //UUID generates unique id
          title:req.body.title,
          text:req.body.text
      };
